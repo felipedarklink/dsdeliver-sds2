0a5c754e53b04077a63add68a760d05c8e30e685
Felipe Silva <felipeosilva@hotmail.com>
2021-01-04 15:31:08 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsdelivier/DsdelivierApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdelivier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdelivierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdelivierApplication.class, args);
+	}
+
+}
