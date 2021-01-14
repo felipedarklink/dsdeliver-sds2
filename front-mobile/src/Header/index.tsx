029e4cc03ac6ab31d09e5dfcc1e4a932dd2dea0e
Felipe Silva <felipeosilva@hotmail.com>
2021-01-14 15:15:36 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,14 +1,26 @@
 import { OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { useNavigation } from '@react-navigation/native';
 import { processFontFamily } from 'expo-font';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+  
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+      navigation.navigate('Home');
+  }
+
+  
   return (
+    <TouchableWithoutFeedback onPress={handleOnPress}>
     <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
         <Text style={styles.text}>DS Delivery</Text>
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
